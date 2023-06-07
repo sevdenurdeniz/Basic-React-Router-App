9c2be1aaa83c2362793e60558679041328191dd2
Sevdenur Deniz <denizsevdenur@gmail.com>
2023-06-07 19:35:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Nav";
+import Hakkimizda from "./Hakkimizda";
+import Uyeler from "./Uyeler";
+import UyeDetay from "./UyeDetay";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hakkimizda" element={<Hakkimizda />} />
+          <Route path="/uyeler" element={<Uyeler />} />
+          <Route path="/uyeler/:id" element={<UyeDetay />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+const Home = () => {
+return(
+  <div>
+  <h3>Anasayfa</h3>
+</div>
+)
+};
 
 export default App;
